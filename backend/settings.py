c2f45133d4617922cf4c26e09faf2f396c72de09
moussaabbou <moussaabbou625@gmail.com>
2025-06-18 12:50:42 +0200

    modifer stockage cloud photo

--- backend/settings.py
+++ backend/settings.py
@@ -92,8 +92,20 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+INSTALLED_APPS += [
+    'cloudinary',
+    'cloudinary_storage',
+]
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 # المفتاح التلقائي
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -5,10 +5,14 @@ class Admin(models.Model):
     surname = models.CharField(max_length=100)
     email = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=255)
-    profile_picture = models.ImageField(upload_to='admin_pics/', null=True, blank=True)  # صورة الإدمن
+    profile_picture = models.ImageField(
+        upload_to='admin_pics/',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.surname}"
 
 class Seller(models.Model):
     name = models.CharField(max_length=100)
@@ -19,24 +23,24 @@ class Seller(models.Model):
     birth_date = models.DateField()
     password = models.CharField(max_length=255)
     profile_picture = models.ImageField(
-        upload_to='seller_pics/', 
-        null=True, 
-        blank=True, 
-        default='seller_pics/PIO.jpg'  # تعيين صورة افتراضية إذا لم يتم رفع صورة
+        upload_to='seller_pics/',
+        null=True,
+        blank=True,
+        default='https://res.cloudinary.com/<cloud_name>/image/upload/v1234567890/seller_pics/PIO.jpg'
     )
 
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.surname}"
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     category = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)  # السعر
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name='products')
-    image1 = models.ImageField(upload_to='product_images/', null=True, blank=True)  # الصورة الأولى
-    image2 = models.ImageField(upload_to='product_images/', null=True, blank=True)  # الصورة الثانية
-    image3 = models.ImageField(upload_to='product_images/', null=True, blank=True)  # الصورة الثالثة
+    image1 = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    image2 = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    image3 = models.ImageField(upload_to='product_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
