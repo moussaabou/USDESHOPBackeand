d98972f48dc6c7187a33cb3a059b5b94dc83f29d
moussaabbou <moussaabbou625@gmail.com>
2025-06-24 00:05:21 +0200

    commit - UsedShop full project

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,16 +1,14 @@
+
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class Admin(models.Model):
     name = models.CharField(max_length=100)
     surname = models.CharField(max_length=100)
     email = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=255)
-    profile_picture = models.ImageField(
-        upload_to='admin_pics/',
-        null=True,
-        blank=True
-    )
-
+    profile_picture = CloudinaryField('image', folder='admin_pics/', null=True, blank=True)
+    
     def __str__(self):
         return f"{self.name} {self.surname}"
 
@@ -22,12 +20,11 @@ class Seller(models.Model):
     address = models.CharField(max_length=255)
     birth_date = models.DateField()
     password = models.CharField(max_length=255)
-    profile_picture = models.ImageField(
-        upload_to='seller_pics/',
-        null=True,
-        blank=True,
-        default='https://res.cloudinary.com/<cloud_name>/image/upload/v1234567890/seller_pics/PIO.jpg'
-    )
+    # other fields...
+    profile_picture = CloudinaryField('image', 
+                                     folder='seller_pics/',
+                                     null=True,
+                                     blank=True)
 
     def __str__(self):
         return f"{self.name} {self.surname}"
@@ -38,9 +35,9 @@ class Product(models.Model):
     category = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name='products')
-    image1 = models.ImageField(upload_to='product_images/', null=True, blank=True)
-    image2 = models.ImageField(upload_to='product_images/', null=True, blank=True)
-    image3 = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    image1 = CloudinaryField('image', folder='product_images/', null=True, blank=True)
+    image2 = CloudinaryField('image', folder='product_images/', null=True, blank=True)
+    image3 = CloudinaryField('image', folder='product_images/', null=True, blank=True)
 
     def __str__(self):
         return self.name
